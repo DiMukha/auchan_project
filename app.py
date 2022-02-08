c5c5c639fe42fd8398a462a33c464a11aa26302f
DiMukha <lutique2012@gmail.com>
2022-02-09 00:00:32 +0200

    add allow_redirects

--- app.py
+++ app.py
@@ -8,17 +8,17 @@ from load_file import load_pickle
 if __name__ == '__main__':
 
     data = load_pickle(Config.FILE_PATH)
-
-
     extractor = URLExtract()
+
     for row in data:
         url = extractor.find_urls(row)
         if url:
-            print(url)
-            if len(url) > 1:
-                print(f"LEN URL = {len(url)}")
             try:
-                print(requests.head(url[0], timeout=10))
+                session = requests.Session()
+                response = requests.head(url[0], allow_redirects=True, timeout=10)
+                print(response.url)
+                print(url)
+                print(response.url == url[0])
             except requests.exceptions.RequestException as ex:
                 print(ex)
             continue
